4a6b020bda44e974c6e96e4ff00e89bf912a61a1
unknown <segottlieb@697-334-Sauron.sf.k12.internal>
2014-09-23 10:07:14 -0700

    Created program Chemotaxis, updated index.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 int bacRed = (int)(Math.random()*255);
 int bacGreen = (int)(Math.random()*255);
 int bacBlue = (int)(Math.random()*255);
 Bacteria [] colony;   
-void setup()   
+public void setup()   
 {    
 	size(600, 600);
 	colony = new Bacteria[500];
@@ -11,7 +27,7 @@ void setup()
 		colony[i] = new Bacteria();
 	}
 }   
-void draw()   
+public void draw()   
 {   
 	background(bacRed,bacGreen,bacBlue);
 	for (int i = 0; i < colony.length; i++) 
@@ -33,12 +49,12 @@ class Bacteria
 		bacGreen = (int)(Math.random()*255);
 		bacBlue = (int)(Math.random()*255);
 	}
-	void move()
+	public void move()
 	{
 		bacX = bacX + (int)(Math.random()*10-5);
 		bacY = bacY + (int)(Math.random()*10-5);
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		bacSize = 13;
@@ -49,7 +65,7 @@ class Bacteria
 			bacFade = 255;
 		}			
 	}
-	void follow()
+	public void follow()
 	{
 		if(mousePressed) 
 		{
@@ -71,4 +87,13 @@ class Bacteria
 			}
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
